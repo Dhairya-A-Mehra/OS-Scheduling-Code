f358e4ce66a52158bdef3e2ca616dfae0e625b36
Dhairya Mehra <mpdhairyamehra000942@gmail.com>
2024-05-01 19:21:22 +0530

    Update Round Robin Algorithm.c

--- scheduling algorithms/Round Robin Dhairya/Round Robin Algorithm.c
+++ scheduling algorithms/Round Robin Dhairya/Round Robin Algorithm.c
@@ -1,99 +1,60 @@
-#include <stdio.h>
-#include <limits.h>
-
-struct Process {
-    int AT, BT, ST[20], WT, FT, TAT, pos;
-};
-
-int quant;
+#include<stdio.h>
 
 int main() {
-    int n, i, j;
-    printf("Enter the no. of processes: ");
-    scanf("%d", &n);
-    struct Process p[n];
-    printf("Enter the quantum: \n");
-    scanf("%d", &quant);
-    printf("Enter the process numbers: \n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &p[i].pos);
-    printf("Enter the Arrival time of processes: \n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &p[i].AT);
-    printf("Enter the Burst time of processes: \n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &p[i].BT);
+    int i, np, qt, sq = 0;
+    int bt[10], rem_bt[10], wt[10], tat[10];
+    float awt = 0, atat = 0;
 
-    int c = n, s[n][20];
-    float time = 0, mini = INT_MAX, b[n], a[n];
+    printf("Enter number of processes: ");
+    scanf("%d", &np);
 
-    int index = -1;
-    for (i = 0; i < n; i++) {
-        b[i] = p[i].BT;
-        a[i] = p[i].AT;
-        for (j = 0; j < 20; j++) {
-            s[i][j] = -1;
-        }
+    printf("Enter time quantum: ");
+    scanf("%d", &qt);
+
+    printf("Enter the burst time of each process: ");
+    for(i = 0; i < np; i++) {
+        scanf("%d", &bt[i]);
+        rem_bt[i] = bt[i]; // Initialize remaining burst time
     }
 
-    int tot_wt = 0, tot_tat = 0;
-    int flag = 0;
-
-    while (c != 0) {
-        mini = INT_MAX;
-        flag = 0;
-
-        for (i = 0; i < n; i++) {
-            float p = time + 0.1;
-            if (a[i] <= p && mini > a[i] && b[i] > 0) {
-                index = i;
-                mini = a[i];
-                flag = 1;
+    // Process each process using Round Robin scheduling
+    while(1) {
+        int completed = 1; // Flag to check if all processes are completed
+        for(i = 0; i < np; i++) {
+            if(rem_bt[i] > 0) {
+                completed = 0; // At least one process is not completed
+                if(rem_bt[i] > qt) {
+                    sq += qt;
+                    rem_bt[i] -= qt;
+                } else {
+                    sq += rem_bt[i];
+                    rem_bt[i] = 0;
+                    tat[i] = sq; // Set turnaround time when process completes
+                }
             }
         }
-
-        if (!flag) {
-            time++;
-            continue;
-        }
-
-        j = 0;
-        while (s[index][j] != -1) {
-            j++;
-        }
-
-        if (s[index][j] == -1) {
-            s[index][j] = time;
-            p[index].ST[j] = time;
-        }
-
-        if (b[index] <= quant) {
-            time += b[index];
-            b[index] = 0;
-        } else {
-            time += quant;
-            b[index] -= quant;
-        }
-
-        if (b[index] > 0) {
-            a[index] = time + 0.1;
-        }
-
-        if (b[index] == 0) {
-            c--;
-            p[index].FT = time;
-            p[index].WT = p[index].FT - p[index].AT - p[index].BT;
-            tot_wt += p[index].WT;
-            p[index].TAT = p[index].BT + p[index].WT;
-            tot_tat += p[index].TAT;
-        }
+        if(completed) // If all processes are completed
+            break;
     }
 
-    double avg_wt = tot_wt / (double)n;
-    double avg_tat = tot_tat / (double)n;
+    // Calculate waiting time and turnaround time for each process
+    for(i = 0; i < np; i++) {
+        wt[i] = tat[i] - bt[i];
+        awt += wt[i];
+        atat += tat[i];
+    }
 
-    printf("The average wait time is: %.2f\n", avg_wt);
-    printf("The average TurnAround time is: %.2f\n", avg_tat);
+    // Calculate average waiting time and average turnaround time
+    awt /= np;
+    atat /= np;
+
+    // Display process details and averages
+    printf("\nProcess\tBurst Time\tTurnaround Time\tWaiting Time\n");
+    for(i = 0; i < np; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\n", i + 1, bt[i], tat[i], wt[i]);
+    }
+    printf("\nAvg Waiting Time: %.2f\n", awt);
+    printf("Avg Turnaround Time: %.2f\n", atat);
 
     return 0;
 }
